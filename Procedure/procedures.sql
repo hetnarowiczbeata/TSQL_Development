bc51bcaa7de48a298466191c27f7c31a07f46d06
Beata <hetnarowiczbeata@gmail.com>
2026-08-24 20:34:33 +0200

    procedures are in progress

--- Procedure/procedures.sql
+++ Procedure/procedures.sql
@@ -69,3 +69,86 @@ END
 
 
 EXEC usp_productsales @REVENUE=7202
+
+--11.1** Create `dbo.usp_GetProductSales` with a `@ProductID` parameter.
+
+CREATE OR ALTER PROCEDURE dbo.usp_GetProductSales
+   @ProductID INT 
+AS
+BEGIN
+    SELECT 
+	p.ProductID,
+	SUM(sod.LineTotal) REV
+	FROM Sales.SalesOrderHeader soh
+    LEFT JOIN Sales.SalesOrderDetail sod ON soh.SalesOrderID=sod.SalesOrderID
+    LEFT JOIN Production.Product p ON sod.ProductID=p.ProductID
+	WHERE p.ProductID=@ProductID
+	GROUP BY p.ProductID
+END
+
+EXEC dbo.usp_GetProductSales @ProductID=707
+
+--11.2** Create `dbo.usp_GetSalesByDate` with `@DateFrom` and `@DateTo` parameters.
+
+CREATE OR ALTER PROCEDURE dbo.usp_GetSalesByDate 
+        @DateFrom DATE,
+		@DateTo DATE
+
+AS
+BEGIN
+     SELECT 
+	 CAST(soh.OrderDate AS DATE) AS OrderDate,
+	 soh.SalesOrderID,
+	 SUM(sod.LineTotal) REV
+	FROM Sales.SalesOrderHeader soh
+    LEFT JOIN Sales.SalesOrderDetail sod ON soh.SalesOrderID=sod.SalesOrderID
+	WHERE 
+	CAST(soh.OrderDate AS DATE)>=@DateFrom 
+	AND CAST(soh.OrderDate AS DATE)<=@DateTo
+	GROUP BY CAST(soh.OrderDate AS DATE),soh.SalesOrderID
+END
+
+EXEC dbo.usp_GetSalesByDate  @DateFrom='2023-05-01', @DateTo='2024-05-01'
+
+--11.3** Create `dbo.usp_ProductSalesSummary` with a `@ProductID` parameter.
+
+CREATE OR ALTER PROCEDURE dbo.usp_ProductSalesSummary
+	@ProductID INT
+AS
+BEGIN
+    SELECT
+        ProductID,
+        SUM(OrderQty) AS Qty,
+        SUM(LineTotal) AS revenue,
+        COUNT(DISTINCT SalesOrderID) AS orders
+    FROM Sales.SalesOrderDetail
+    WHERE ProductID = @ProductID
+    GROUP BY ProductID;
+END;
+
+EXEC dbo.usp_ProductSalesSummary @ProductID=707
+
+--11.4** Create `dbo.usp_TopSellingProducts` with a `@TopN` parameter.
+CREATE OR ALTER PROCEDURE dbo.usp_TopSellingProducts
+	@TopN INT
+AS
+ BEGIN
+     SELECT TOP(@TopN)
+	 sod.ProductID,
+	 SUM(sod.LineTotal) rev
+	 FROM Sales.SalesOrderHeader soh
+     LEFT JOIN Sales.SalesOrderDetail sod ON soh.SalesOrderID=sod.SalesOrderID
+	 GROUP BY sod.ProductID
+	 ORDER BY rev DESC
+END 
+
+EXEC dbo.usp_TopSellingProducts @TopN=5
+
+
+* [ ] **11.5** Create `dbo.usp_OrdersAboveValue` with a `@MinimumValue` parameter.
+* [ ] **11.6** Create a procedure returning monthly sales results.
+* [ ] **11.7** Create a procedure using a temporary table.
+* [ ] **11.8** Create a procedure using a CTE.
+* [ ] **11.9** Create a procedure with an optional parameter.
+* [ ] **11.10** Add parameter validation to at least one procedure.
+* [ ] **11.11** Add `TRY...CATCH` error handling to at least one procedure.
